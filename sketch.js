3718ec6e165a6e4948366974317d70af670f684f
LegendaryGamer2 <90922505+LegendaryGamer2@users.noreply.github.com>
2021-10-18 11:00:05 -0600

    added preload for image

--- sketch.js
+++ sketch.js
@@ -1,10 +1,17 @@
-// Project Title
-// Your Name
+// Angry Bird Game
+// Tamoor Ali Ahmed
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+
+
+let pig;
+
+function preload(){
+  pig = loadImage("assets/PIG.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
